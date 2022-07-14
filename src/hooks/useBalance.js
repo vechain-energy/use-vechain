090655352c62c151c478b59fde3c016ea6e18508
Mario Micklisch <mm@hpm.agency>
2022-07-14 14:06:39 +0200

    Add ability for fetch balance on-demand

--- src/hooks/useBalance.js
+++ src/hooks/useBalance.js
@@ -10,18 +10,30 @@ export function useBalance (address) {
   const [vet, setVet] = useState(0)
   const [vtho, setVtho] = useState(0)
 
+  async function balanceOf (address, raw) {
+    const accountVisitor = connex.thor.account(address)
+    const { balance, energy } = await accountVisitor.get()
+    if (raw) {
+      return { vet: balance, vtho: energy }
+    }
+
+    return {
+      vet: ethers.utils.formatEther(new ethers.utils.BigNumber(balance)),
+      vtho: ethers.utils.formatEther(new ethers.utils.BigNumber(energy))
+    }
+  }
+
   useEffect(() => {
     if (!connex || !address) { return }
-
-    const accountVisitor = connex.thor.account(address)
-    accountVisitor.get().then(({ balance, energy }) => {
-      setVet(ethers.utils.formatEther(new ethers.utils.BigNumber(balance)))
-      setVtho(ethers.utils.formatEther(new ethers.utils.BigNumber(energy)))
+    balanceOf(address).then(({ vet, vtho }) => {
+      setVet(vet)
+      setVtho(vtho)
     })
   }, [connex, head, address])
 
   return {
     vet,
-    vtho
+    vtho,
+    balanceOf
   }
 }
