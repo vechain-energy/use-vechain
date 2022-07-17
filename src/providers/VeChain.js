d27a111c0dc09411bf27d4d35a16028056b8cca9
Mario Micklisch <mm@hpm.agency>
2022-07-17 13:56:56 +0200

    Add support for mobile wallet

--- src/providers/VeChain.js
+++ src/providers/VeChain.js
@@ -12,16 +12,30 @@ export const VeChainProvider = ({ children, config, options }) => {
   const [account, setAccount] = useLocalStorage('account')
   const [defaultOptions, setDefaultOptions] = useState({})
 
+  function getSystemPreferredConnex () {
+    if (window.connex && getNetworkByGenesisId(window.connex.thor.genesis.id) === config.network) {
+      return window.connex
+    }
+
+    return connex
+  }
+
   const connect = useCallback(async (payloadOrContent = 'identification') => {
+    const connex = getSystemPreferredConnex()
     const payload = typeof (payloadOrContent) === 'object' ? { ...payloadOrContent } : { type: 'text', content: payloadOrContent }
     const certificate = {
       purpose: 'agreement',
       payload
     }
 
+    if (connex?.version?.split('.')[0] === '1') {
+      const result = await connex.vendor.sign('cert').request(certificate)
+      setAccount(result.annex.signer)
+      return result
+    }
+
     const result = await connex.vendor.sign('cert', certificate).request()
     setAccount(result.annex.signer)
-
     return result
   }, [])
 
@@ -34,6 +48,7 @@ export const VeChainProvider = ({ children, config, options }) => {
   }, [options])
 
   const waitForTransactionId = useCallback(async function waitForTransactionId (id) {
+    const connex = getSystemPreferredConnex()
     const transaction = connex.thor.transaction(id)
     let receipt = await transaction.getReceipt()
     while (!receipt) {
@@ -56,10 +71,16 @@ export const VeChainProvider = ({ children, config, options }) => {
   }, [])
 
   const submitTransaction = useCallback(async function submitTransaction (clauses, options = {}) {
+    const connex = getSystemPreferredConnex()
     const transaction = connex.vendor.sign('tx', clauses)
     const { delegateTest, ...optionsWithDefaults } = { ...defaultOptions, ...options }
     for (const key of Object.keys(optionsWithDefaults)) {
       /* eslint-disable no-useless-call */
+      if (connex?.version?.split('.')[0] === '1' && key === 'delegate') {
+        transaction[key].call(transaction, (args) => postJSON(optionsWithDefaults.delegate, args))
+        continue
+      }
+
       if (Array.isArray(optionsWithDefaults[key])) {
         transaction[key].call(transaction, ...optionsWithDefaults[key])
       } else {
@@ -83,11 +104,16 @@ export const VeChainProvider = ({ children, config, options }) => {
         }
       })
 
-      const { success, message, errors } = await postJSON(delegateTest, { raw: `0x${testTransaction.encode().toString('hex')}`, origin }, [200])
+      const { success, message, errors } = await postJSON(delegateTest, { raw: `0x${testTransaction.encode().toString('hex')}`, origin })
       if (!success) {
         console.error('fee delegation test failed', errors)
         throw new FeeDelegationError(message || 'fee delegation rejected', errors)
       }
+    }
+
+    if (connex?.version?.split('.')[0] === '1') {
+      const { txid } = await transaction.request(clauses)
+      return txid
     }
 
     const { txid } = await transaction.request()
@@ -97,6 +123,13 @@ export const VeChainProvider = ({ children, config, options }) => {
   return <VeChainContext.Provider value={{ connex, connect, disconnect, account, config, options, submitTransaction, waitForTransactionId }}>{children}</VeChainContext.Provider>
 }
 
+function getNetworkByGenesisId (id) {
+  const NETWORK_IDS = {
+    '0x00000000851caf3cfdb6e899cf5958bfb1ac3413d346d43539627e6be7ec1b4a': 'main',
+    '0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127': 'test'
+  }
+  return NETWORK_IDS[id] || 'local'
+}
 class FeeDelegationError extends Error {
   constructor (message, errors) {
     super(message)
