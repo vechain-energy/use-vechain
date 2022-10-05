857ed2ccf215342b30e63434e686de27aa563a63
Salvatore Distefano <Yukark@users.noreply.github.com>
2022-10-05 12:50:59 +0200

    Implemented tokens price information

--- src/hooks/useTokens.js
+++ src/hooks/useTokens.js
@@ -4,16 +4,39 @@ import bent from 'bent'
 import getNetworkByGenesisId from '../providers/VeChain/getNetworkByGenesisId'
 
 const iconUri = 'https://vechain.github.io/token-registry/assets/'
-const getTokenRegistry = bent('https://vechain.github.io/token-registry/', 'GET', 'json')
 
-export function useTokens () {
+const getTokenRegistry = bent('https://vechain.github.io/token-registry/', 'GET', 'json')
+const getTokenPrice = bent('https://api.vexchange.io/v1/tokens', 'GET', 'json')
+
+export function useTokens(includePrice) {
   const { connex } = useContext(VeChainContext)
   const [tokens, setTokens] = useState([])
 
   useEffect(() => {
     if (!connex) { return }
+
     const network = getNetworkByGenesisId(connex.thor.genesis.id)
-    getTokenRegistry(`${network}.json`).then(tokens => setTokens(tokens.map(token => ({ ...token, icon: `${iconUri}${token.icon}` }))))
+
+    getTokenRegistry(`${network}.json`)
+      .then((tokens) => {
+        if (includePrice) {
+          return getTokenPrice().then((prices) => [
+            tokens,
+            Object.values(prices)
+          ])
+        }
+
+        return [tokens, undefined]
+      })
+      .then(([tokens, prices]) => {
+        setTokens(
+          tokens.map((token) => ({
+            ...token,
+            icon: `${iconUri}${token.icon}`,
+            usdPrice: prices?.find((price) => token.symbol === price.symbol)?.usdPrice
+          }))
+        )
+      })
   }, [connex])
 
   return {
