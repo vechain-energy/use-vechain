66ea730a83e316dafa30ea8c6d5c22a1c19d95ee
Mario Micklisch <mm@hpm.agency>
2022-10-09 09:01:05 +0200

    Add ability for more options

--- src/hooks/useTokens.js
+++ src/hooks/useTokens.js
@@ -8,36 +8,42 @@ const iconUri = 'https://vechain.github.io/token-registry/assets/'
 const getTokenRegistry = bent('https://vechain.github.io/token-registry/', 'GET', 'json')
 const getTokenPrice = bent('https://api.vexchange.io/v1/tokens', 'GET', 'json')
 
-export function useTokens(includePrice) {
+export function useTokens (options) {
   const { connex } = useContext(VeChainContext)
   const [tokens, setTokens] = useState([])
+
+  async function addUsdPrices (tokens) {
+    const prices = await getTokenPrice()
+    const priceBySymbol = Object.values(prices).reduce((priceBySymbol, price) => {
+      priceBySymbol[price.symbol] = price.usdPrice
+      return priceBySymbol
+    }, {})
+
+    return tokens.map(token => {
+      return {
+        ...token,
+        usdPrice: priceBySymbol[token.symbol]
+      }
+    })
+  }
+  const loadTokens = async (network, options = {}) => {
+    const rawTokens = await getTokenRegistry(`${network}.json`)
+
+    let tokens = rawTokens.map(token => ({ ...token, icon: `${iconUri}${token.icon}` }))
+
+    if (options.usdPrice) {
+      tokens = await addUsdPrices(tokens)
+    }
+
+    setTokens(tokens)
+  }
 
   useEffect(() => {
     if (!connex) { return }
 
     const network = getNetworkByGenesisId(connex.thor.genesis.id)
-
-    getTokenRegistry(`${network}.json`)
-      .then((tokens) => {
-        if (includePrice) {
-          return getTokenPrice().then((prices) => [
-            tokens,
-            Object.values(prices)
-          ])
-        }
-
-        return [tokens, undefined]
-      })
-      .then(([tokens, prices]) => {
-        setTokens(
-          tokens.map((token) => ({
-            ...token,
-            icon: `${iconUri}${token.icon}`,
-            usdPrice: prices?.find((price) => token.symbol === price.symbol)?.usdPrice
-          }))
-        )
-      })
-  }, [connex])
+    loadTokens(network, options)
+  }, [connex, JSON.stringify(options)])
 
   return {
     tokens
